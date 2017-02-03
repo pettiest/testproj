d9d15ddcfd6e223b1148ab83f566b6574bc99c23
arthi <arthiresume@gmail.com>
2017-02-02 22:18:17 -0800

    Commit new folder structure

---
+++ src/main/java/com/mycompany/app/MyTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by arthi on 2/2/17.
+ */
+public class MyTest {
+    final static Logger logger = Logger.getLogger(MyTest.class);
+
+    public static void main(String[] args) {
+        System.out.println("Value"+MyTest.class);
+        logger.debug("This is debug : " );
+
+    }
+
+}
